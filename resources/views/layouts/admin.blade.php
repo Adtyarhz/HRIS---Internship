8b9ea285a57f16fd08d37fbb79bfdf4254a96d45
adtyarhz <149128711+Adtyarhz@users.noreply.github.com>
2025-07-02 16:34:56 +0700

    update tampilan admin layout
    menghapus footer, update tampilan css

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -190,11 +190,6 @@
             <!-- /.content -->
         </div>
         <!-- /.content-wrapper -->
-
-        <footer class="main-footer">
-            <strong>Copyright &copy; {{ date('Y') }} <a href="#">Your Company</a>.</strong> All rights
-            reserved.
-        </footer>
     </div>
     <!-- ./wrapper -->
 
