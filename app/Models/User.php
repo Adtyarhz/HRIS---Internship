7651653d0c817a895300ee280039f9b82185e9d0
adtyarhz <149128711+Adtyarhz@users.noreply.github.com>
2025-10-20 10:34:44 +0700

    model dan relasinya

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
@@ -59,14 +60,38 @@ class User extends Authenticatable
         return $this->hasMany(CareerProjection::class, 'created_by');
     }
 
-    public function editRequests()
+    public function editRequests(): HasMany
     {
         return $this->hasMany(EmployeeEditRequest::class, 'employee_id');
     }
-    // App\Models\User.php
-public function scopeAdmins($query)
-{
-    return $query->whereIn(\DB::raw('LOWER(role)'), ['hc', 'superadmin']);
-}
+    
+    public function scopeAdmins($query)
+    {
+        return $query->whereIn(DB::raw('LOWER(role)'), ['hc', 'superadmin']);
+    }
 
+    // --- Relasi ke ChangeDataRequest ---
+
+    public function changeDataRequestsMade(): HasMany
+    {
+        return $this->hasMany(ChangeDataRequest::class, 'requested_by');
+    }
+
+    public function changeDataRequestsChecked(): HasMany
+    {
+        return $this->hasMany(ChangeDataRequest::class, 'checked_by');
+    }
+
+    public function changeDataRequestsApproved(): HasMany
+    {
+        return $this->hasMany(ChangeDataRequest::class, 'approved_by');
+    }
+
+    /**
+     * Relasi untuk request yang ditolak oleh user ini.
+     */
+    public function changeDataRequestsRejected(): HasMany
+    {
+        return $this->hasMany(ChangeDataRequest::class, 'rejected_by');
+    }
 }
